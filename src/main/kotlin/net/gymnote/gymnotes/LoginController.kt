b968fcdd5f396b0b0dd8c236e42de87252fda7ca
YL <2582167435@qq.com>
2021-03-16 21:50:29 +0800

    测试

--- src/main/kotlin/net/gymnote/gymnotes/LoginController.kt
+++ src/main/kotlin/net/gymnote/gymnotes/LoginController.kt
@@ -12,6 +12,6 @@ class LoginController {
 
     @RequestMapping("/login")
     fun login(user: User): String? {
-        return user.toString() + "lzl 1"
+        return user.toString() + "lzl 2"
     }
 }
